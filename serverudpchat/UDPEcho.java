8c693912feaf249c04121bca60c84b0a3710221c
lucian-pavel-peano-5A <56472164+lucian-pavel-peano-5A@users.noreply.github.com>
2019-12-09 13:38:24 +0100

    Update UDPEcho.java
    Gli ultimi 10 messaggi vengono salvati

--- serverudpchat/UDPEcho.java
+++ serverudpchat/UDPEcho.java
@@ -45,6 +45,7 @@ public class UDPEcho implements Runnable {
     }
 
     public void run() {
+        LinkedList<String> last10messages = new LinkedList<String>();
         DatagramPacket answer; //datagram usato per creare il pacchetto di risposta
         byte[] buffer = new byte[8192]; //buffer per contenere il messaggio ricevuto o da inviare
         // creo un un datagramma UDP usando il buffer come contenitore per i messaggi
@@ -74,6 +75,14 @@ public class UDPEcho implements Runnable {
                 if(message == "quit") {
                     //client si e' rimosso da chat, lo rimuovo da lista dei client connessi
                     clients.remove(clientID);
+                }
+                if(last10messages.size()<10)
+                        //Aggiungo il messaggio alla LinkedList
+                	last10messages.add(message);
+                else {
+                	//Se la LinkedList contiene già 10 messaggi, rimuovo il primo messaggio inserito ed aggiungo quello nuovo
+                	last10messages.removeLast();
+                	last10messages.addFirst(message);
                 }
 
                 //invio il messaggio ricevuto a tutti i client connessi al server
